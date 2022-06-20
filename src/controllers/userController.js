9d59686deb010e9d9926484e882384ce63a7aae3
Breno Albuquerque <brenoralbu@hotmail.com>
2022-06-20 13:39:09 -0300

    feat: Rota post register funcionando

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,1 +1,15 @@
 const userService = require('../services/userService');
+
+const register = async (req, res, next) => {
+  try {
+    const token = await userService.register(req.body);
+  
+    res.status(201).json({ token });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  register,
+};
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,1 +1,16 @@
 const { User } = require('../database/models');
+const { generateToken } = require('../helpers/JwtToken');
+
+const register = async ({ displayName, email, password, image }) => {
+  const newUser = await User.create({
+    displayName, email, password, image,
+  });
+
+  const token = generateToken(newUser.dataValues);
+
+  return token;
+};
+
+module.exports = {
+  register,
+};
