6a4aa44924f2d956ae51d9ddeee5793d2cb8bd99
Breno Albuquerque <brenoralbu@hotmail.com>
2022-06-20 13:07:37 -0300

    feat(controllers): Adiciona bloco try..catch no login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,9 +1,13 @@
 const loginService = require('../services/loginService');
 
-const login = async (req, res) => {
-  const token = await loginService.login(req.body);
-
-  res.status(200).json({ token });
+const login = async (req, res, next) => {
+  try {
+    const token = await loginService.login(req.body);
+  
+    res.status(200).json({ token });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
