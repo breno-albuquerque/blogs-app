08631ee51a5a81afe6aa6a049b4d5709d8810a45
Breno Albuquerque <brenoralbu@hotmail.com>
2022-06-22 08:44:05 -0300

    feat(services): Cria instância genérica do axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import { getUsers } from "./services/requests";
 
 function App() {
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const users = await getUsers();
+
+    }
+
+    fetchUsers();
+  }, [])
+  
+
   return (
     <h1>Hello from App</h1>
   );
