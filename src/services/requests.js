aac8a4d1e2be60afcb646e910e490f4960d26177
Breno Albuquerque <brenoralbu@hotmail.com>
2022-06-22 08:44:40 -0300

    feat(services): GET "/users" functionando

--- src/services/requests.js
+++ src/services/requests.js
@@ -5,3 +5,8 @@ const instance = axios.create({
   headers: { 'authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZGlzcGxheU5hbWUiOiJMZXdpcyBIYW1pbHRvbiIsImVtYWlsIjoibGV3aXNoYW1pbHRvbkBnbWFpbC5jb20iLCJpYXQiOjE2NTU4OTc0NTl9.ui8LGF4MLOhJuH_K-qcHfeKFdE6WwH9r4tzGX6BVpuE' }
 })
 
+export const getUsers = async () => {
+  const response = await instance.get('/user');
+  console.log(response)
+  return response.data;
+}
