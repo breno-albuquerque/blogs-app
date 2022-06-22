f32bba1d32894ff6958c99e580190e6f2c36a56a
Breno Albuquerque <brenoralbu@hotmail.com>
2022-06-22 09:10:22 -0300

    fix(middlewares): Agora validateToken retorna a função no block catch

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -3,13 +3,14 @@ const CustomError = require('../helpers/CustomError');
 
 function validateToken(req, res, next) {
   const token = req.headers.authorization;
+  
   if (!token) throw new CustomError('401', 'Token not found');
-
+  
   try {
     const decrypted = verifyToken(token);
     req.user = decrypted;
   } catch (error) {
-    res.status(401).json({ message: 'Expired or invalid token' });
+    return res.status(401).json({ message: 'Expired or invalid token' });
   }
 
   next();
