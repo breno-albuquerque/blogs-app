5f5be1d00cd9e6697163b7f5b03ea9ccfa628ff0
Breno Albuquerque <brenoralbu@hotmail.com>
2022-06-22 13:49:46 -0300

    feat(components): Adiciona botões e redirecionamento de rotas no Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,39 @@
 import React from 'react';
-/* import { styled } from 'styled-components';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router'
 
 const Container = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 
-` */
+  border: solid 1px black;
+  height: 300px;
+
+  min-width: 300px;
+  max-width: 600px;
+
+  margin: 0 auto;
+
+  position: absolute;
+  top: 0; bottom: 0;
+  left: 0; right: 0;
+  margin: auto;
+`
 
 function Home() {
+  const navigate = useNavigate();
+
+  const handleClick = (endpoint) => {
+    navigate(`/${endpoint}`)
+  }
+
   return (
-    <div>Home</div>
+    <Container>
+      <button onClick={ () => handleClick('register') } >Register</button>
+      <button onClick={ () => handleClick('login') }>Login</button>
+    </Container>
   )
 }
 
