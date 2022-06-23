565bc95c12ee2906a00bc1e246a74371aefcee21
Breno Albuquerque <brenoralbu@hotmail.com>
2022-06-23 09:26:21 -0300

    style(css): Adiciona estilo para o form de register em registerComponents

--- src/components/RegisterComponent.jsx
+++ src/components/RegisterComponent.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import { MDBInput } from 'mdb-react-ui-kit';
+import React from 'react';
+import TogglePages from './TogglePages';
 
 function Register() {
   return (
-    <div>Register</div>
-  )
+    <section className="vh-100">
+
+      <div className="container-fluid h-custom">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-md-9 col-lg-6 col-xl-5">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+              className="img-fluid"
+              alt="Sample"
+            />
+          </div>
+
+          <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+            <TogglePages />
+            <form>
+              <div className="divider d-flex align-items-center my-4">
+                <p className="text-center fw-bold mx-3 mb-0">Sign-in</p>
+              </div>
+
+              <div className="form-outline mb-4">
+                <MDBInput
+                  autoComplete="off"
+                  onChange={handleChange}
+                  value={displayName}
+                  type="text"
+                  name="email"
+                  className="form-control form-control-lg"
+                  label="Email"
+                />
+              </div>
+
+              <div className="form-outline mb-4">
+                <MDBInput
+                  autoComplete="off"
+                  onChange={handleChange}
+                  value={email}
+                  type="email"
+                  name="email"
+                  className="form-control form-control-lg"
+                  label="Email"
+                />
+              </div>
+
+              <div className="form-outline mb-3">
+                <MDBInput
+                  type="password"
+                  id="form3Example4"
+                  className="form-control form-control-lg"
+                  label="Password"
+                  autoComplete="off"
+                  onChange={handleChange}
+                  value={password}
+                  name="password"
+                />
+              </div>
+
+              <div className="form-outline mb-4">
+                <MDBInput
+                  autoComplete="off"
+                  onChange={handleChange}
+                  value={image}
+                  type="text"
+                  name="image"
+                  className="form-control form-control-lg"
+                  label="Image"
+                />
+              </div>
+
+              <div className="text-center text-lg-start mt-4 pt-2">
+                <button
+                  type="button"
+                  onClick={handleClick}
+                  className="btn btn-primary btn-lg"
+                  style={{ paddingLeft: '2.5rem', paddingRight: '2.5rem' }}
+                >
+                  Register
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Register
+export default Register;
