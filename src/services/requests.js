abb7d31dff8ab2a4117853f6ae8f7c2093035a06
Breno Albuquerque <brenoralbu@hotmail.com>
2022-06-24 16:20:52 -0300

    feat(services): Adiciona blocos try..catch para interceptar erros

--- src/services/requests.js
+++ src/services/requests.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import handleError from '../helpers/handleError';
 
 const makeInstance = (token) => {
   const instanceToken = axios.create({
@@ -14,68 +15,104 @@ const instance = axios.create({
 });
 
 export const getUsers = async (id = null) => {
-  let endpoint;
+  try {
+    let endpoint;
 
-  if (id) endpoint = `/user/${id}`;
-  else endpoint = '/user';
+    if (id) endpoint = `/user/${id}`;
+    else endpoint = '/user';
 
-  const response = await instance.get(endpoint);
-  return response.data;
+    const response = await instance.get(endpoint);
+    return response.data;
+  } catch (error) {
+    return handleError(error);
+  }
 };
 
 export const getBlogPosts = async (token, q = null) => {
-  let endpoint;
+  try {
+    let endpoint;
 
-  if (q) endpoint = `/post/search?q=${q}`;
-  else endpoint = '/post';
+    if (q) endpoint = `/post/search?q=${q}`;
+    else endpoint = '/post';
 
-  const instanceToken = makeInstance(token);
-  const response = await instanceToken.get(endpoint);
-  return response.data;
+    const instanceToken = makeInstance(token);
+    const response = await instanceToken.get(endpoint);
+    return response.data;
+  } catch (error) {
+    return handleError(error);
+  }
 };
 
 export const getCategories = async (token) => {
-  const instanceToken = makeInstance(token);
-  const response = await instanceToken.get('/categories');
-  return response.data;
+  try {
+    const instanceToken = makeInstance(token);
+    const response = await instanceToken.get('/categories');
+    return response.data;
+  } catch (error) {
+    return handleError(error);
+  }
 };
 
 export const register = async (userData) => {
-  const response = await instance.post('/user', userData);
-  return response.data.token;
+  try {
+    const response = await instance.post('/user', userData);
+    return response.data.token;
+  } catch (error) {
+    return handleError(error);
+  }
 };
 
 export const login = async (userData) => {
-  const response = await instance.post('/login', userData);
-  return response.data.token;
+  try {
+    const response = await instance.post('/login', userData);
+    return response.data.token;
+  } catch (error) {
+    return handleError(error);
+  }
 };
 
 export const publish = async (token, postData) => {
-  const instanceToken = makeInstance(token);
-  const response = await instanceToken.post('/post', postData);
-  return response.data;
+  try {
+    const instanceToken = makeInstance(token);
+    const response = await instanceToken.post('/post', postData);
+    return response.data;
+  } catch (error) {
+    return handleError(error);
+  }
 };
 
 export const edit = async (token, postData, id) => {
-  const instanceToken = makeInstance(token);
+  try {
+    const instanceToken = makeInstance(token);
 
-  const editData = {
-    title: postData.title,
-    content: postData.content,
-  };
+    const editData = {
+      title: postData.title,
+      content: postData.content,
+    };
 
-  const response = await instanceToken.put(`/post/${id}`, editData);
-  return response.data;
+    const response = await instanceToken.put(`/post/${id}`, editData);
+    return response.data;
+  } catch (error) {
+    return handleError(error);
+  }
 };
 
 export const createCategory = async (token, category) => {
-  const instanceToken = makeInstance(token);
-  const response = await instanceToken.post('/categories', { name: category });
-  return response.data;
+  try {
+    const instanceToken = makeInstance(token);
+    const response = await instanceToken.post('/categories', { name: category });
+    return response.data;
+  } catch (error) {
+    return handleError(error);
+  }
 };
 
 export const deleteBlogPost = async (token, userId) => {
-  const instanceToken = makeInstance(token);
-  const response = await instanceToken.delete(`/post/${userId}`);
-  return response.data;
+  try {
+    const instanceToken = makeInstance(token);
+    const response = await instanceToken.delete(`/post/${userId}`);
+    return response.data;
+  } catch (error) {
+    return handleError(error);
+  }
 };
