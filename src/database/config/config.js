7b70ae12c723b21d84b7c53a97b88f6aa6fe4f31
Breno Albuquerque <brenoralbu@hotmail.com>
2022-06-25 15:50:09 -0300

    fix: Remove códigos e arquivos da Trybe

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,32 +1,32 @@
-require('dotenv').config();
-
-const environment = process.env.NODE_ENV || 'test';
-
-const suffix = {
-  dev: '-dev',
-  development: '-dev',
-  test: '-test',
-};
-
-const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'blogs-api'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '1234',
-  dialect: 'mysql',
-  dialectOptions: {
-    timezone: 'Z',
-  },
-  logging: process.env.DEBUG !== 'false',
-};
+const dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = {
   development: {
-    ...options,
+    username: process.env.MYSQL_USER || 'root',
+    port: process.env.MYSQL_PORT || '3306',
+    password: process.env.MYSQL_PASSWORD || '1234',
+    database: process.env.MYSQL_DB_NAME || 'blogs-api-dev',
+    host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
+    dialect: 'mysql',
+    logging: process.env.DEBUG !== 'false',
+    dialectOptions: {
+      timezone: 'Z',
+    },
   },
   test: {
-    ...options,
+    username: process.env.USER,
+    password: process.env.DATABASE,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql'
   },
-};
+  production: {
+    username: process.env.USER,
+    password: process.env.DATABASE,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  }
+}
+
