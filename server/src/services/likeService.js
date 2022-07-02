0f714c18d960edd070300daa371b5ca9fede5f34
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-02 14:10:48 -0300

    feat(server): Progride o service da rota post "/like"

--- server/src/services/likeService.js
+++ server/src/services/likeService.js
@@ -1,9 +1,15 @@
 const { Like } = require('../database/models');
+const CustomError = require('../helpers/CustomError');
 
-const post = async () => {
+const post = async (postId, userId) => {
+  // Verifica se esse usuário ainda não curtiu esse post
+  const rows = await Like.findAll({ where: { postId, userId } });
+  if (rows.length !== 0) throw new CustomError(409, 'User already liked this post');
 
-}
+  const result = await Like.create({ postId, userId });
+  return result;
+};
 
 module.exports = {
-  post
-}
+  post,
+};
