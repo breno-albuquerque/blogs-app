af9e3a004ef486ba3173fb2a5a0238ec6a4b4c83
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-02 14:06:12 -0300

    feat(server): Controller da rota post "/like" agora pega o Id do post por parâmetro

--- server/src/controllers/likeController.js
+++ server/src/controllers/likeController.js
@@ -2,8 +2,8 @@ const likeService = require('../services/likeService');
 
 const post = async (req, res, next) => {
   try {
-    const { id } = req.user;
-    await likeService.post(id);
+    const { id } = req.params;
+    await likeService.post(id, req.user.id);
     res.status(201).end();
   } catch (error) {
     next(error);
