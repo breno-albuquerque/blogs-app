35e815946410b6c9c46f3e5b26423cab301b9321
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-04 09:08:31 -0300

    refactor(server): Muda nome das variáveis de ambiente

--- server/src/database/config/config.js
+++ server/src/database/config/config.js
@@ -4,26 +4,26 @@ dotenv.config();
 
 module.exports = {
   development: {
-    username: process.env.MYSQL_USERNAME,
-    port: process.env.MYSQL_PORT,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB,
-    host: process.env.MYSQL_HOST,
-    dialect: 'mysql',
+    username: process.env.DB_USERNAME,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DB,
+    host: process.env.DB_HOST,
+    dialect: 'DB',
   },
   test: {
     username: process.env.USER,
     password: process.env.DATABASE,
     database: 'database_test',
     host: '127.0.0.1',
-    dialect: 'mysql',
+    dialect: 'DB',
   },
   production: {
-    username: process.env.MYSQL_USERNAME,
-    port: process.env.MYSQL_PORT,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB,
-    host: process.env.MYSQL_HOST,
+    username: process.env.DB_USERNAME,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: process.env.DIALECT,
     dialectOptions: {
       ssl: {
