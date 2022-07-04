806111386413ab7c2404681a11832c77d2f83008
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-04 09:23:08 -0300

    fix(server): hard code production dialect como postgres

--- server/src/database/config/config.js
+++ server/src/database/config/config.js
@@ -24,7 +24,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
