677752419ea19baa0315fdd9ab48959c63692eb6
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-04 07:48:44 -0300

    refactor(server): Corrige erros de eslint dos controllers

--- server/src/controllers/loginController.js
+++ server/src/controllers/loginController.js
@@ -3,7 +3,7 @@ const loginService = require('../services/loginService');
 const login = async (req, res, next) => {
   try {
     const token = await loginService.login(req.body);
-  
+
     res.status(200).json({ token });
   } catch (error) {
     next(error);
