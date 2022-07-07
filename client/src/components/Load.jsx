4f1914af64dea74bf6fbb2ec2fd952c1c3fdc916
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-07 09:53:48 -0300

    feat(client): Adiciona animação no componente Load

--- client/src/components/Load.jsx
+++ client/src/components/Load.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Box = styled.div`
+  position: absolute;
+  top: calc(50% - 75px);
+  left: calc(50% - 75px);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  animation: ${rotate} infinite 5s linear;
+
+  border-radius: 100%;
+  width: 150px;
+  height: 150px;
+  z-index: 10;
+  border: dotted #ef3f46 5px;
+`;
 
 function Load() {
   return (
-    <div>Loading</div>
+    <Box>
+      <p>Loading</p>
+    </Box>
   );
 }
 
